23d210e0a17167568e8be5943f3211306f172e59
Katarzyna Pieniazek <pieniazek.k@gmail.com>
2017-07-17 16:45:05 +0200

    geocoding from input and firebase

--- js/app.js
+++ js/app.js
@@ -5,51 +5,15 @@ import './../css/style.scss';
 
 
 $(()=>{ // DOM Content Loaded
-
+  // catching HTML elements
   let buttonFind = $('#find');
   let mapId = $('#map')[0]; // jQuery: return Object - I have to pull out div elem
+  // initialize variables
+  let map;
+  let geocoder;
 
-  // making map
-  function initMap() {
-        let centrum = {lat:  52.229676 , lng: 21.012229}; //my focus on start point
-        let map = new google.maps.Map( mapId, { //create new map elem in #map
-          zoom: 13,
-          center: centrum
-        });
-
-        // geocoding obj
-        var geocoder = new google.maps.Geocoder();
-
-        // function converting address
-        function geocodeAddress(geocoder, map2) {
-          var myAddress = $('#where').val() + ",Warszawa" ; // get street from input and add Warsaw to address
-          geocoder.geocode({'address': myAddress}, function(results, status) {
-            if (status === 'OK') {
-                map2.setCenter(results[0].geometry.location);
-                var marker2 = new google.maps.Marker({
-                    map: map2,
-                    position: results[0].geometry.location
-                });
-            }else {
-                alert('Geocode was not successful for the following reason: ' + status);
-            }
-          });
-        }
-
-        // event dla buttonFind - dodawanie markera z szukaną lokalizacją
-        buttonFind.on("click" , (event) => {
-          geocodeAddress(geocoder , map); // decoding address from input + marker
-        });
-  };
-
-  // set key and initialize map
-  $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBYDAQpbxOhU7n07Tc_flZ8XxtzsyF-lm0")
-    .done(function(data) {
-      initMap();
-    });
-
-  // variable with config data to Firebase
-  var config = {
+  // variable with config data to get to Firebase
+  let config = {
       apiKey: "AIzaSyA0PL06i_Fdi70FkUxOo4I9JToVS-632U8",
       authDomain: "my-awesome-coffee.firebaseapp.com",
       databaseURL: "https://my-awesome-coffee.firebaseio.com",
@@ -70,6 +34,80 @@ $(()=>{ // DOM Content Loaded
       $(elem).next().text("ul. " + snap.val()[i].adress);
     });
   });
+
+  // making map
+  function initMap() {
+        let centrum = {lat:  52.229676 , lng: 21.012229}; //my focus on start point
+        map = new google.maps.Map( mapId, { //create new map elem in #map
+          zoom: 13,
+          center: centrum
+        });
+        // geocoding obj (initialized on top)
+        geocoder = new google.maps.Geocoder();
+  };
+
+  // function converting address
+  function geocodeAddress(geocoder, map2) {
+    let myAddress = $('#where').val() + ",Warszawa" ; // get street from input and add Warsaw to address
+    geocoder.geocode({'address': myAddress}, function(results, status) {
+      if (status === 'OK') {
+          map2.setCenter(results[0].geometry.location);
+          let marker2 = new google.maps.Marker({
+              map: map2,
+              position: results[0].geometry.location
+          });
+      }else {
+          alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  // get street from firebase and add Warsaw to address
+  let coffeeAddress;
+  db.on('value' , (snap) => {  coffeeAddress = snap.val()[2].adress});
+
+  //function seting cafes from firebase
+  function geocodeCafes(geocoder, map2, coffeeAddress) {
+    geocoder.geocode({'address': coffeeAddress}, function(results, status) {
+      if (status === 'OK') {
+          var marker3 = new google.maps.Marker({
+              map: map2,
+              position: results[0].geometry.location
+          });
+      }else {
+          alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  // events and  map initialization
+  $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBYDAQpbxOhU7n07Tc_flZ8XxtzsyF-lm0")
+    .done(function(data) {
+
+        // event for buttonFind - add markers after set input address
+        buttonFind.on("click" , (event) => {
+          geocodeAddress(geocoder , map); // decoding address from input + marker
+          geocodeCafes(geocoder , map, coffeeAddress); // decoding address from firebase + marker
+        });
+        // event for press enter
+        $('html').on("keyup" , (event) => {
+          const enter = 13;
+          if(event.keyCode == enter){
+            geocodeAddress(geocoder , map); // decoding address from input + marker
+            geocodeCafes(geocoder , map, coffeeAddress); // decoding address from firebase + marker
+          }
+        });
+
+        initMap();
+    });
+
+
+
+
+
+
+
+
 
 
 
--- js/out.js
+++ js/out.js
@@ -3495,49 +3495,14 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 $(function () {
   // DOM Content Loaded
-
+  // catching HTML elements
   var buttonFind = $('#find');
   var mapId = $('#map')[0]; // jQuery: return Object - I have to pull out div elem
+  // initialize variables
+  var map = void 0;
+  var geocoder = void 0;
 
-  // making map
-  function initMap() {
-    var centrum = { lat: 52.229676, lng: 21.012229 }; //my focus on start point
-    var map = new google.maps.Map(mapId, { //create new map elem in #map
-      zoom: 13,
-      center: centrum
-    });
-
-    // geocoding obj
-    var geocoder = new google.maps.Geocoder();
-
-    // function converting address
-    function geocodeAddress(geocoder, map2) {
-      var myAddress = $('#where').val() + ",Warszawa"; // get street from input and add Warsaw to address
-      geocoder.geocode({ 'address': myAddress }, function (results, status) {
-        if (status === 'OK') {
-          map2.setCenter(results[0].geometry.location);
-          var marker2 = new google.maps.Marker({
-            map: map2,
-            position: results[0].geometry.location
-          });
-        } else {
-          alert('Geocode was not successful for the following reason: ' + status);
-        }
-      });
-    }
-
-    // event dla buttonFind - dodawanie markera z szukaną lokalizacją
-    buttonFind.on("click", function (event) {
-      geocodeAddress(geocoder, map); // decoding address from input + marker
-    });
-  };
-
-  // set key and initialize map
-  $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBYDAQpbxOhU7n07Tc_flZ8XxtzsyF-lm0").done(function (data) {
-    initMap();
-  });
-
-  // variable with config data to Firebase
+  // variable with config data to get to Firebase
   var config = {
     apiKey: "AIzaSyA0PL06i_Fdi70FkUxOo4I9JToVS-632U8",
     authDomain: "my-awesome-coffee.firebaseapp.com",
@@ -3558,6 +3523,73 @@ $(function () {
       // set adresses from firebase (to the next elems)
       $(elem).next().text("ul. " + snap.val()[i].adress);
     });
+  });
+
+  // making map
+  function initMap() {
+    var centrum = { lat: 52.229676, lng: 21.012229 }; //my focus on start point
+    map = new google.maps.Map(mapId, { //create new map elem in #map
+      zoom: 13,
+      center: centrum
+    });
+    // geocoding obj (initialized on top)
+    geocoder = new google.maps.Geocoder();
+  };
+
+  // function converting address
+  function geocodeAddress(geocoder, map2) {
+    var myAddress = $('#where').val() + ",Warszawa"; // get street from input and add Warsaw to address
+    geocoder.geocode({ 'address': myAddress }, function (results, status) {
+      if (status === 'OK') {
+        map2.setCenter(results[0].geometry.location);
+        var marker2 = new google.maps.Marker({
+          map: map2,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  // get street from firebase and add Warsaw to address
+  var coffeeAddress = void 0;
+  db.on('value', function (snap) {
+    coffeeAddress = snap.val()[2].adress;
+  });
+
+  //function seting cafes from firebase
+  function geocodeCafes(geocoder, map2, coffeeAddress) {
+    geocoder.geocode({ 'address': coffeeAddress }, function (results, status) {
+      if (status === 'OK') {
+        var marker3 = new google.maps.Marker({
+          map: map2,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  // events and  map initialization
+  $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBYDAQpbxOhU7n07Tc_flZ8XxtzsyF-lm0").done(function (data) {
+
+    // event for buttonFind - add markers after set input address
+    buttonFind.on("click", function (event) {
+      geocodeAddress(geocoder, map); // decoding address from input + marker
+      geocodeCafes(geocoder, map, coffeeAddress); // decoding address from firebase + marker
+    });
+    // event for press enter
+    $('html').on("keyup", function (event) {
+      var enter = 13;
+      if (event.keyCode == enter) {
+        geocodeAddress(geocoder, map); // decoding address from input + marker
+        geocodeCafes(geocoder, map, coffeeAddress); // decoding address from firebase + marker
+      }
+    });
+
+    initMap();
   });
 }); // my coffee places with detail info
 
