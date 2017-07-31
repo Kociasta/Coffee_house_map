2d9d5f2c6233e9e6d7028e626654d9c250e752ad
Katarzyna Pieniazek <pieniazek.k@gmail.com>
2017-07-31 10:08:15 +0200

    hours - sup

--- js/hours.js
+++ js/hours.js
@@ -1,39 +1,30 @@
+// function which returns opening hours of chosen cafe in actual day of week (obj. new Date() return "today")
+
 let getHours = function(allCafes){
+  // actually allCafes = allCafes[i]
 
-let day = ""
-
-switch(new Date().getDay()) {
-    case 1:
-      day = "mon";
-      return allCafes[1].hours().mon;
-      break
-    case 2:
-      day = "tue";
-      return allCafes[1].hours().tue;
-      break
-    case 3:
-      day = "wed";
-      return allCafes[1].hours().wed;
-      break
-    case 4:
-      day = "thu";
-      return allCafes[1].hours().thu;
-      break
-    case 5:
-      day = "fri";
-      return allCafes[1].hours().fri;
-    case 6:
-      day = "sat";
-      return allCafes[1].hours().sat;
-      break
-    case 0:
-      day = "sun";
-      return allCafes[1].hours().sun;
-      break
-}
-
-
-
+  switch(new Date().getDay()) {
+      case 1:
+        return allCafes[1].hours().mon;
+        break
+      case 2:
+        return allCafes[1].hours().tue;
+        break
+      case 3:
+        return allCafes[1].hours().wed;
+        break
+      case 4:
+        return allCafes[1].hours().thu;
+        break
+      case 5:
+        return allCafes[1].hours().fri;
+      case 6:
+        return allCafes[1].hours().sat;
+        break
+      case 0:
+        return allCafes[1].hours().sun;
+        break
+  }
 
 }
 
--- js/setNamesetAddress.js
+++ js/setNamesetAddress.js
@@ -2,12 +2,7 @@ import getHours from './hours.js';
 
 let moduleSetNameSetAddress = (function(allCafes) {
 
-
-
-  let setNameAndAddress = function(allCafes){
-
-
-
+  let _setNameAndAddress = function(allCafes){
 
     $(".cafe-name").each((i , elem) => { // find elements with class cafe-name
         $(elem).text(allCafes[i][1].name);
@@ -17,29 +12,41 @@ let moduleSetNameSetAddress = (function(allCafes) {
     });
 
     // here - changing hours
-    $(".cafe-hours-from").each((i , elem) => { // find elements with class cafe-name
+    $(".cafe-hours-from").each((i , elem) => { // find elements with class cafe-hours-from
 
-      if(getHours(allCafes[i]).from === "") {
+      if(getHours(allCafes[i]).from === "") { // if this day cafe is closed
+
         $(elem).html(`<span class="closed">ZAMKNIĘTE</span>`);
         $(elem).css("width" , `50%`);
 
-      } else {
-        var from = parseInt(getHours(allCafes[i]).from , 10);
+      } else {                               // if it is open
 
-        $(elem).css("width" , `${(from-6)*5}%`);
-        // $(elem).text(getHours(allCafes[i]).from);
-        $(elem).html(`<span>${from}:00</span>`);
-        // console.log(getHours(allCafes[i]).from);
+        let from = getHours(allCafes[i]).from ; // 2 type of data in base -> "8" or "7:30"
+
+        if(from.length === 1 ){
+          $(elem).css("width" , `${(parseInt(from,10)-6)*5}%`);
+          $(elem).html(`<span>${from}<sup>00</sup></span>`);
+
+        } else {
+          let min = from[2] + from[3]; //return string made of 2 numbers e.g. from[2]=3, from[3]=0 -> min=30
+          $(elem).css("width" , `${(parseInt(from,10)-6)*5}%`); // parse only 1st number e.g. 7:30 -> 7
+          $(elem).html(`<span>${parseInt(from,10)}<sup>${min}</sup></span>`);
+        }
+
       }
-
-
     });
-    $(".cafe-hours-to").each((i , elem) => { // find elements with class cafe-name
-      if(getHours(allCafes[i]).to === "") {
-        $(elem).html(`<span> </span>`);
+
+    $(".cafe-hours-to").each((i , elem) => { // find elements with class cafe-hours-to
+
+      if(getHours(allCafes[i]).to === "") { // if cafe is close
+
+        $(elem).html(`<span> </span>`); // clearing previous elem
         $(elem).css("width" , `0`);
-      } else {
-        let to = parseInt(getHours(allCafes[i]).to ,10);
+
+      } else {                              // if cafe i open
+
+        let to = getHours(allCafes[i]).to;
+
         if(to < 16) {
           $(elem).css("width" , `${(24-6)*5}%`);
 
@@ -52,12 +59,11 @@ let moduleSetNameSetAddress = (function(allCafes) {
         // console.log(to);
       }
 
-
     });
   }
 
   return {
-    set: setNameAndAddress
+    set: _setNameAndAddress
   }
 
 })();
